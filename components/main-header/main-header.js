9af479faa225ba3a34d2e1aa13ff45c681cf20c4
unwokolo <150948104+unwokolo@users.noreply.github.com>
2024-10-30 20:23:46 -0500

    addition of loading.js file

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,12 +1,9 @@
-
-
 import Link from "next/link";
 import logoImg from '@/assets/logo.png';
 import classes from "./main-header.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
 import NavLink from "./nav-link";
-
 
 export default function MainHeader() {
     return (
